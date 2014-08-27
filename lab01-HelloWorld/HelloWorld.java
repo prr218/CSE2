1128ba306a46bc212ddfef37666d344444f7f9d2
prr218 <prr218@lehigh.edu>
2014-08-27 16:46:01 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+//Priyokti Rana
+//First Programming Project (yay!)
+//Hello World Java Program
+
+//  define a class
+public class HelloWorld{
+    
+// add main methos
+    public static void main(String[] args) {
+        
+//  print statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+
+
+    }
+}
