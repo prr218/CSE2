c96fece98506e496986b394e4fe10fdb41b21d4d
prr218 <prr218@lehigh.edu>
2014-08-28 02:05:26 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Priyokti Rana
+//hw01
+//WelcomeClass
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main methods
+    public static void main(String[] args) {
+        
+//  print statement with box on it
+    System.out.println("        "+"-----------");
+    System.out.println("        "+"| WELCOME |");
+    System.out.println("        "+"-----------");
+    System.out.println("     "+"^  ^  ^  ^  ^  ^");
+    System.out.println("      "+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\");
+    System.out.println("|-8--6--1--7--3--3--1--3--8-|");
+    System.out.println("      "+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/");
+    System.out.println("     "+"v  v  v  v  v  v");
+    System.out.println("I went paragliding in Nepal this summer, and we crash landed in a rice field. It was awesome.");
+
+    //first hw assignment done yay! now onto figuring out whether this code works...took me a while to figure it out...
+    
+    }
+    
+
+
+}
