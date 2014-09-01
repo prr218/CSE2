09b4a2e2c3a0b5e3f76822f3952f729889173ec4
prr218 <prr218@lehigh.edu>
2014-09-01 03:35:16 +0000

    updated WelcomeClass

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -12,11 +12,11 @@ public class WelcomeClass{
     System.out.println("        "+"-----------");
     System.out.println("        "+"| WELCOME |");
     System.out.println("        "+"-----------");
-    System.out.println("      "+"^  ^  ^  ^  ^  ^");
-    System.out.println("      "+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\");
-    System.out.println("|-8--6--1--7--3--3--1--3--8-|");
-    System.out.println("      "+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/");
-    System.out.println("      "+"v  v  v  v  v  v");
+    System.out.println("       "+"^  ^  ^  ^  ^  ^");
+    System.out.println("     "+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\"+"/"+" "+"\\");
+    System.out.println("    "+"|-p--r--r--2--1--8-|");
+    System.out.println("     "+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/"+"\\"+" "+"/");
+    System.out.println("       "+"v  v  v  v  v  v");
     System.out.println("I went paragliding in Nepal this summer, and we crash landed in a rice field. It was awesome.");
 
     //first hw assignment done yay! now onto figuring out whether this code works...took me a while to figure it out...
