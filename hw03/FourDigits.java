06cf83e00e1cbaefa6b577932a5c622c09ca3467
prr218 <prr218@lehigh.edu>
2014-09-16 16:15:21 +0000

    FourDigits

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -6,12 +6,27 @@
 // CSE02 with Professor Brian Chen
 // WRITE THIS This program...
 
+//statement needed for a scanner to be functional
+import java.util.Scanner;
+
 // define a class
 public class FourDigits{
     
 // add main method 
     public static void main(String[] args) {
-        
+    
+    Scanner myScanner; //declaring scanner as variable
+    
+    myScanner = new Scanner ( System.in ); //statement calling the scanner constructor
+    
+    //statement prompting user for the double
+    System.out.println("Enter a double, and I will display the four digits to the right of the decimal point-"); // statement promtping user to enter number
+    double x = myScanner.nextDouble(); // enables user to enter number
+    System.out.print("The four digits are " + ((int)(((double)(x)%((int)(x)))*10))); // extracts digit from 10s place
+    System.out.print(((int)(((double)(x)%((int)(x)))*100))%10); // extracts digit from 100s place
+    System.out.print((((int)(((double)(x)%((int)(x)))*1000)))%10); // extracts digit from 1000s place
+    System.out.print((((int)(((double)(x)%((int)(x)))*10000)))%10); //extracts digit from 10000s place
+    
 // 
-    }
-}
+    } //end main method
+} //end of class
--- hw03/Root.java
+++ hw03/Root.java
@@ -6,9 +6,31 @@
 // CSE02 with Professor Brian Chen
 // WRITE THIS This program
 
+//statement needed for a scanner to be functional
+import java.util.Scanner;
+
 // define a class
 public class Root{
     
 // add main methods
     public static void main(String[] args) {
-}
+        
+    Scanner myScanner; //declaring scanner as variable
+    
+    myScanner = new Scanner ( System.in ); //statement calling the scanner constructor
+    
+    //statement promting user for the double
+    System.out.print("Enter a double, and I will print its cube root ");
+    double x = myScanner.nextDouble();
+    
+    //statement showing the cube root
+    double guess = x/3;
+    System.out.println("The cube root is " + guess + ":");
+    System.out.println((guess*guess*guess+x)/(3*guess*guess));
+    System.out.println((2*guess*guess*guess+x)/(3*guess*guess));
+    System.out.println((3*guess*guess*guess+x)/(3*guess*guess));
+    System.out.println((4*guess*guess*guess+x)/(3*guess*guess));
+    
+} //end main method
+        
+}//end of class
