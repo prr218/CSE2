d113551892743b5b3556b9ec2ff3bfbb63aed73e
prr218 <prr218@lehigh.edu>
2014-09-16 21:51:50 +0000

    Root

--- hw03/Root.java
+++ hw03/Root.java
@@ -24,12 +24,12 @@ public class Root{
     double x = myScanner.nextDouble();
     
     //statement showing the cube root
-    double guess = x/3;
-    System.out.println("The cube root is " + guess + ":");
-    System.out.println((guess*guess*guess+x)/(3*guess*guess));
-    System.out.println((2*guess*guess*guess+x)/(3*guess*guess));
-    System.out.println((3*guess*guess*guess+x)/(3*guess*guess));
-    System.out.println((4*guess*guess*guess+x)/(3*guess*guess));
+    double guess = x/3; // first crude guess
+    double guess2 = (guess*guess*guess+x)/(3*guess*guess); //second guess based on first guess
+    double guess3 =(2*guess2*guess2*guess2+x)/(3*guess2*guess2); //third guess for cube root based on second
+    double guess4 = (3*guess3*guess3*guess3+x)/(4*guess3*guess3); //as the guesses go on, they are becoming more accurate, this one is based on the third
+    double guess5 = (4*guess3*guess3*guess3+x)/(5*guess3*guess3); //fifth guess based on the fourth
+    System.out.println("The cube root is " + guess5 + ": "+ guess5 +" "+guess5+" = "+(guess5*guess5*guess5)); //statement showing fifth guess and the actual cube of the guess 
     
 } //end main method
         
